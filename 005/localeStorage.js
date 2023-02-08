e1d154c7e0b7d2d41ac38429ffb33e2f27780d6e
sgrazys <s.grazys@gmail.com>
2023-02-08 23:26:09 +0200

    localStorage

---
+++ 005/localeStorage.js
@@ -0,0 +1,25 @@
+// LOCALE STORAGE. 
+/* 
+Trumpai apie local storage. Kokia problema sprendzia. 
+Kintamojo reiksme pasilieka perkrovus puslapi, perkrovus kompa ir t.t.
+Local storage sago pasirinktus kintamuosius. Tai yra narsykles savybe.
+*/
+
+//Sugalvojau kad skyblue butu isaugota ir ji visuomet butu
+
+// let color = 'skyblue';
+
+// i local storage setinu spalva. myFavoriteColor yra tarsi indexas(key), o value yra priskirtas kintamsis su turiniu.
+
+// localStorage.setItem('myFavoriteColor', color);
+
+//istrinti itema is localstorage
+localStorage.removeItem('myFavoriteColor');
+
+//isitraukiu kintamaji. Nors color ir localStorage.setItem uzkomentinti, taciau narsykleje vistiek gausiu skyblue, nes jis buvo irasytas i local storage
+console.log(localStorage.getItem('myFavoriteColor'));
+
+
+// TURINT KAZKOKI REIKSME localStorage mes ja gauname, neturint jos mes gauname NULL. PRISIMINTI. Reiskias tokio key localStorage nebuvo.
+
+//
---
+++ 005/nice.js
@@ -0,0 +1,43 @@
+// Padarysime spalvos selectoriu. Pasirinksime spalva ir nudazysime background'a.
+
+const htmlBody = document.querySelector('body');
+const color = document.querySelector('[type=color]');
+const size = document.querySelector('[type=range]');
+
+console.log(size)
+console.log(htmlBody)
+
+color
+    .addEventListener('change', e => {
+        htmlBody.style.backgroundColor = e.target.value;
+        settings.color = e.target.value;
+
+        localStorage.setItem('bc', JSON.stringify(settings));
+    });
+
+size
+    .addEventListener('change', e => {
+        htmlBody.style.fontSize = e.target.value + 'px';
+        settings.size = e.target.value;
+
+        localStorage.setItem('bc', JSON.stringify(settings));
+    });
+
+
+//Start'as
+let settings = localStorage.getItem('bc');
+if (null === settings) {
+    settings = {
+        color: '#ffffff',
+        size: 16
+    };
+    localStorage.setItem('bc', JSON.stringify(settings));
+} else {
+    settings = JSON.parse(settings)
+}
+htmlBody.style.backgroundColor = settings.color;
+color.value = settings.color;
+
+htmlBody.style.fontSize = settings.size + 'px';
+size.value = settings.size;
+
