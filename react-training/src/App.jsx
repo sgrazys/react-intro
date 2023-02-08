1a07eea91b9fcb417085b749f829a98d40bcb69c
sgrazys <s.grazys@gmail.com>
2023-02-08 16:29:31 +0200

    Rect 1rst lesson

---
+++ react-training/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Racoon from './Components/006/Racoon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Racoon/>
+        <Racoon/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
