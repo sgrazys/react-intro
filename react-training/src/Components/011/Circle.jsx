8c5af699fb4ec8834975eaf235ee907e2a84eb96
sgrazys <s.grazys@gmail.com>
2023-02-21 11:42:38 +0200

    update

--- react-training/src/Components/011/Circle.jsx
+++ react-training/src/Components/011/Circle.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-function Circle({ children }) {
-	const [count, setCount] = useState(0);
+function Circle({children}) {
 
-	// const addCount = (c) => ({ ...c, props: { ...c.props, count: count } });
+    const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<button className='blue' onClick={() => setCount((c) => c + 1)}>
-				Plus 1
-			</button>
-			<div className='circle'>
-				{children.map((n, i) => ({
-					...n,
-					props: { ...n.props, count: count + i },
-				}))}
-			</div>
-		</>
-	);
+    // const addCount = c  =>  ({...c, props: {...c.props, count: count}});
+
+
+    return (
+        <>
+        <button className="blue" onClick={() => setCount(c => c + 1)}>plus 1</button>
+        <div className="circle">
+            {
+                children.map((n, i) => ({...n, props: {...n.props, count: count + i }})) 
+            }
+        </div>
+        </>
+    );
 }
 
 export default Circle;
